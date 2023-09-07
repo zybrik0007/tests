a51d3e642fb4eb99ee7601a40e24c8a0ac23ef41
zybrik0007 <48869019+zybrik0007@users.noreply.github.com>
2023-09-07 10:57:57 +0300

    Update full-ui.spec.js

--- specs/full-ui.spec.js
+++ specs/full-ui.spec.js
@@ -1,12 +1,4 @@
 const {describe} = require('mocha')
 const tests = require('../ui/src/handlers/soft')
 
-describe('Проверка функционала', () => {
-
-    describe('Проверка раздела "Персонал."', () => {
-        tests.divisionUi.add.addDuplicateOneLevel()
-        //tests.additionalDataUi.staff.main()
-        //tests.additionalDataUi.staff.add.typeSelectDeleteBox()
-        //tests.additionalDataUi.visitor.main()
-    });
-});
+tests.divisionUi.add.addDuplicateOneLevel()
