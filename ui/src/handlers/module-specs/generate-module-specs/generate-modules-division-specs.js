80e6bfa250a46f4b3c95d2d87c9ee35997d7100f
Alex zuybrovskiy <zybrik0007@rambler.ru>
2023-09-14 14:27:08 +0300

    Test Teamcity

--- ui/src/handlers/module-specs/generate-module-specs/generate-modules-division-specs.js
+++ ui/src/handlers/module-specs/generate-module-specs/generate-modules-division-specs.js
@@ -1,114 +1,115 @@
 const path = require('path')
 const {generationSpec, removeSpecs, generationJSON, integrateJSON} = require('../../other/module-spec-generation');
+const pathFile = 'console.log = function () {};\nrequire(\'../../../../../ui/src/handlers/module-specs\')'
 
 const arrayDivision = [
     {
         name: 'divisionUi.add.addMinParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addMinParams()`
+        script: `${pathFile}.divisionUi.add.addMinParams()`
     },
     {
         name: 'divisionUi.add.addMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addMaxParams()`
+        script: `${pathFile}.divisionUi.add.addMaxParams()`
     },
     {
         name: 'divisionUi.add.addFormsMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addFormsMaxParams()`
+        script: `${pathFile}.divisionUi.add.addFormsMaxParams()`
     },
     {
         name: 'divisionUi.add.addIncludeProgression.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addIncludeProgression()`
+        script: `${pathFile}.divisionUi.add.addIncludeProgression()`
     },
     {
         name: 'divisionUi.add.addDuplicateOneLevel.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addDuplicateOneLevel()`
+        script: `${pathFile}.divisionUi.add.addDuplicateOneLevel()`
     },
     {
         name: 'divisionUi.add.addDuplicateTwoLevel.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addDuplicateTwoLevel()`
+        script: `${pathFile}.divisionUi.add.addDuplicateTwoLevel()`
     },
     {
         name: 'divisionUi.add.addNoName.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.add.addNoName()`
+        script: `${pathFile}.divisionUi.add.addNoName()`
     },
     {
         name: 'divisionUi.edit.editMinParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editMinParams()`
+        script: `${pathFile}.divisionUi.edit.editMinParams()`
     },
     {
         name: 'divisionUi.edit.editMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editMaxParams()`
+        script: `${pathFile}.divisionUi.edit.editMaxParams()`
     },
     {
         name: 'divisionUi.edit.editAllParamsMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editAllParamsMaxParams()`
+        script: `${pathFile}.divisionUi.edit.editAllParamsMaxParams()`
     },
     {
         name: 'divisionUi.edit.editAllParamsFormsMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editAllParamsFormsMaxParams()`
+        script: `${pathFile}.divisionUi.edit.editAllParamsFormsMaxParams()`
     },
     {
         name: 'divisionUi.edit.editHideShow.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editHideShow()`
+        script: `${pathFile}.divisionUi.edit.editHideShow()`
     },
     {
         name: 'divisionUi.edit.editDuplicateOneLevel.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editDuplicateOneLevel()`
+        script: `${pathFile}.divisionUi.edit.editDuplicateOneLevel()`
     },
     {
         name: 'divisionUi.edit.editDuplicateTwoLevel.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.edit.editDuplicateTwoLevel()`
+        script: `${pathFile}.divisionUi.edit.editDuplicateTwoLevel()`
     },
     {
         name: 'divisionUi.delete.deleteLevelOne.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteLevelOne()`
+        script: `${pathFile}.divisionUi.delete.deleteLevelOne()`
     },
     {
         name: 'divisionUi.delete.deleteLevelOneFailed.spec.js',
-        script: `require('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteLevelTwo()`
+        script: `${pathFile}.divisionUi.delete.deleteLevelTwo()`
     },
     {
         name: 'divisionUi.delete.deleteLevelTwo.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteLevelOneFailed()`
+        script: `${pathFile}.divisionUi.delete.deleteLevelOneFailed()`
     },
     {
         name: 'divisionUi.delete.deleteLevelTwoFailed.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteLevelTwoFailed()`
+        script: `${pathFile}.divisionUi.delete.deleteLevelTwoFailed()`
     },
     {
         name: 'divisionUi.delete.deleteLevelTwoFailed.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteStaffFailed()`
+        script: `${pathFile}.divisionUi.delete.deleteStaffFailed()`
     },
     {
         name: 'divisionUi.delete.deleteLevelTwoFailed.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.delete.deleteVisitorFailed()`
+        script: `${pathFile}.divisionUi.delete.deleteVisitorFailed()`
     },
     {
         name: 'divisionUi.service.addDivision.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.service.addDivision()`
+        script: `${pathFile}.divisionUi.service.addDivision()`
     },
     {
         name: 'divisionUi.service.editDivision.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.service.editDivision()`
+        script: `${pathFile}.divisionUi.service.editDivision()`
     },
     {
         name: 'divisionUi.service.deleteDivision.spec.js',
-        script: `require('../../../../../ui/src/handlers/module-specs').divisionUi.service.deleteDivision()`
+        script: `${pathFile}.divisionUi.service.deleteDivision()`
     },
     {
         name: 'divisionUi.print.printMinParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.print.printMinParams()`
+        script: `${pathFile}.divisionUi.print.printMinParams()`
     },
     {
         name: 'divisionUi.print.printMaxParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.print.printMaxParams()`
+        script: `${pathFile}.divisionUi.print.printMaxParams()`
     },
     {
         name: 'divisionUi.printTree.printTreeMinParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.printTree.printTreeMaxParams()`
+        script: `${pathFile}.divisionUi.printTree.printTreeMaxParams()`
     },
     {
         name: 'divisionUi.printTree.printTreeMinParams.spec.js',
-        script: `console.log = function () {};\nrequire('../../../../../ui/src/handlers/module-specs').divisionUi.printTree.printTreeMaxParams()`
+        script: `${pathFile}.divisionUi.printTree.printTreeMaxParams()`
     },
 ];
 
