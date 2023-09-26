06968e1bce65281cf194d8fff651c32df3140679
Alex zuybrovskiy <zybrik0007@rambler.ru>
2023-09-26 16:50:11 +0300

    Настройка входящих данных из Teamcity

--- specs/ui/modules/personal/division.js
+++ specs/ui/modules/personal/division.js
@@ -1,3 +1,5 @@
 const tests = require('../../../../ui/src/handlers/module-specs/specs/personal/division-ui');
 
-console.log(process.env);
+console.log('process: ', process);
+console.log('process.env: ', process.env);
+console.log('process.env.b001divisionUi.display: ', process.env.b001divisionUi.display);
