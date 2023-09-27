8d74e4ac42a710b18c860707036d60d8b44ddbf4
Alex zuybrovskiy <zybrik0007@rambler.ru>
2023-09-27 09:44:45 +0300

    Настройка входящих данных из Teamcity

--- specs/ui/modules/personal/division.js
+++ specs/ui/modules/personal/division.js
@@ -1,6 +1,8 @@
 const tests = require('../../../../ui/src/handlers/module-specs/specs/personal/division-ui');
 const closeBrowser = require('../../../../ui/src/handlers/other/closeBrowser');
 
+console.log('process.env: ', process.env);
+
 console.log = () => function () {};
 
 // Отображение
