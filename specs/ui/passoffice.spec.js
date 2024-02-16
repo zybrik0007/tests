ee461575b9634366cdde7fd13e3eea0ca2029c7e
Alex zuybrovskiy <zybrik0007@rambler.ru>
2024-02-16 14:50:20 +0300

    Изменение teamcity.

--- specs/ui/passoffice.spec.js
+++ specs/ui/passoffice.spec.js
@@ -1,7 +1,6 @@
 const tests = require('../../ui/src/handlers/module-specs');
 const closeBrowser = require('../../ui/src/handlers/other/closeBrowser');
 const data = require('../../ui/src/handlers/module-specs/data');
-const decorate = require('../../ui/src/decorates');
 console.log = () => function () {};
 
 // Бюро пропусков / Посетители
@@ -12,13 +11,11 @@ tests.visitorUi.otherVisitorOrder.printTableVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.printBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.printDesignCardVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.exportXLSXVisitorMaxParams();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.currentVisitorMaxParamsFromOrder();
 tests.visitorUi.otherVisitorOrder.archiveVisitorMaxParamsFromOrder();
 tests.visitorUi.otherVisitorOrder.addBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.addBarcodeDuplicateVisitor();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.addCardVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.addCardDuplicateVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.editVisitorMaxParams();
@@ -26,7 +23,6 @@ tests.visitorUi.otherVisitorOrder.toArchiveVisitor();
 
 // Бюро пропусков / Посетители - вкладка - Архив
 tests.visitorUi.otherVisitorArchive.deleteVisitor();
-decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Действующие
 tests.visitorUi.otherVisitorActive.addVisitorMaxParams();
@@ -34,7 +30,6 @@ tests.visitorUi.otherVisitorActive.printTableVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.printBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.printDesignCardVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.exportXLSXVisitorMaxParams();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorActive.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.blockCardVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.unblockCardVisitorMaxParams();
@@ -42,7 +37,6 @@ tests.visitorUi.otherVisitorActive.archiveVisitorMaxParamsFromActive();
 
 // Бюро пропусков / Посетители - вкладка - Архив
 tests.visitorUi.otherVisitorArchive.printTableVisitorMaxParams();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorArchive.exportXLSXVisitorMaxParams();
 tests.visitorUi.otherVisitorArchive.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorArchive.returnVisitorMaxParamsToActive();
@@ -50,7 +44,6 @@ tests.visitorUi.otherVisitorArchive.returnVisitorMaxParamsToActive();
 // Бюро пропусков / Посетители - вкладка - Действующие
 tests.visitorUi.otherVisitorActive.addBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.editVisitorMaxParams();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorActive.toArchiveVisitor();
 
 // Бюро пропусков / Посетители - вкладка - Архив
@@ -60,7 +53,6 @@ tests.visitorUi.otherVisitorArchive.deleteVisitor();
 tests.visitorUi.otherVisitorOrder.importMinParams();
 tests.visitorUi.otherVisitorOrder.importMaxParams();
 tests.visitorUi.otherVisitorOrder.importFailed();
-decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Действующие
 tests.visitorUi.otherVisitorActive.importMinParams();
@@ -70,7 +62,6 @@ tests.visitorUi.otherVisitorActive.importFailed();
 // Бюро пропусков / Посетители - вкладка - Заказанные
 tests.visitorUi.otherVisitorOrder.addDataVisitorFilter();
 tests.visitorUi.otherVisitorOrder.divisionFilterVisitor();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.searchFilterVisitorActive();
 tests.visitorUi.otherVisitorOrder.deleteDataVisitorFilter();
 
@@ -78,7 +69,6 @@ tests.visitorUi.otherVisitorOrder.deleteDataVisitorFilter();
 tests.visitorUi.otherVisitorActive.addDataVisitorFilter();
 tests.visitorUi.otherVisitorActive.divisionFilterVisitor();
 tests.visitorUi.otherVisitorActive.searchFilterVisitorActive();
-decorate.page.base.refresh();
 tests.visitorUi.otherVisitorActive.deleteDataVisitorFilter();
 data.deleteDataVisitor();
 
@@ -87,31 +77,26 @@ data.deleteDataVisitor();
 // Бюро пропусков / Сотрудники
 data.addDataStaff();
 tests.staffUi.otherPass.addStaffMinParams();
-decorate.page.base.refresh();
 tests.staffUi.otherPass.addCardStaffMinParams();
 tests.staffUi.otherPass.deleteCardStaffMinParams();
 tests.staffUi.otherPass.blockCardStaffMinParams();
 tests.staffUi.otherPass.unblockCardStaffMinParams();
 tests.staffUi.otherPass.addBarcodeStaffMinParams();
-decorate.page.base.refresh();
 tests.staffUi.otherPass.deleteBarcodeStaffMinParams();
 tests.staffUi.otherPass.editStaffMaxParams();
 tests.staffUi.otherPass.printTableStaffMaxParams();
 tests.staffUi.otherPass.printBarcodeStaffMaxParams();
 tests.staffUi.otherPass.printDesignCardStaffMaxParams();
-decorate.page.base.refresh();
 tests.staffUi.otherPass.dimissedRestoreStaffMinParams();
 tests.staffUi.otherPass.exportStaffXLSXMaxParams();
 tests.staffUi.otherPass.exportStaffCSVMaxParams();
 tests.staffUi.other.dimissedStaff();
 tests.staffUi.other.deleteStaff();
-decorate.page.base.refresh();
 
 data.addDataStaffFilter();
 tests.staffUi.otherPass.searchFilterStaffActive();
 tests.staffUi.otherPass.divisionFilterStaffActive();
 tests.staffUi.otherPass.cardSearchStaffActive();
 data.deleteDataStaffFilter();
-decorate.page.base.refresh();
 
 closeBrowser();
--- specs/ui/teamcity/passoffice.teamcity.ui.spec.js
+++ specs/ui/teamcity/passoffice.teamcity.ui.spec.js
@@ -1,6 +1,7 @@
 const tests = require('../../../ui/src/handlers/module-specs');
 const closeBrowser = require('../../../ui/src/handlers/other/closeBrowser');
 const data = require('../../../ui/src/handlers/module-specs/data');
+const decorate = require('../../../ui/src/decorates');
 console.log = () => function () {};
 
 // Бюро пропусков / Посетители
@@ -10,16 +11,19 @@ tests.visitorUi.otherVisitorOrder.addVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.printTableVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.printBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.printDesignCardVisitorMaxParams();
+decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.exportXLSXVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.currentVisitorMaxParamsFromOrder();
 tests.visitorUi.otherVisitorOrder.archiveVisitorMaxParamsFromOrder();
 tests.visitorUi.otherVisitorOrder.addBarcodeVisitorMaxParams();
+decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.addBarcodeDuplicateVisitor();
 tests.visitorUi.otherVisitorOrder.addCardVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.addCardDuplicateVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.editVisitorMaxParams();
 tests.visitorUi.otherVisitorOrder.toArchiveVisitor();
+decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Архив
 tests.visitorUi.otherVisitorArchive.deleteVisitor();
@@ -29,17 +33,20 @@ tests.visitorUi.otherVisitorActive.addVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.printTableVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.printBarcodeVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.printDesignCardVisitorMaxParams();
+decorate.page.base.refresh();
 tests.visitorUi.otherVisitorActive.exportXLSXVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.blockCardVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.unblockCardVisitorMaxParams();
 tests.visitorUi.otherVisitorActive.archiveVisitorMaxParamsFromActive();
+decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Архив
 tests.visitorUi.otherVisitorArchive.printTableVisitorMaxParams();
 tests.visitorUi.otherVisitorArchive.exportXLSXVisitorMaxParams();
 tests.visitorUi.otherVisitorArchive.exportCSVVisitorMaxParams();
 tests.visitorUi.otherVisitorArchive.returnVisitorMaxParamsToActive();
+decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Действующие
 tests.visitorUi.otherVisitorActive.addBarcodeVisitorMaxParams();
@@ -51,6 +58,7 @@ tests.visitorUi.otherVisitorArchive.deleteVisitor();
 
 // Бюро пропусков / Посетители - вкладка - Заказанные
 tests.visitorUi.otherVisitorOrder.importMinParams();
+decorate.page.base.refresh();
 tests.visitorUi.otherVisitorOrder.importMaxParams();
 tests.visitorUi.otherVisitorOrder.importFailed();
 
@@ -58,6 +66,7 @@ tests.visitorUi.otherVisitorOrder.importFailed();
 tests.visitorUi.otherVisitorActive.importMinParams();
 tests.visitorUi.otherVisitorActive.importMaxParams();
 tests.visitorUi.otherVisitorActive.importFailed();
+decorate.page.base.refresh();
 
 // Бюро пропусков / Посетители - вкладка - Заказанные
 tests.visitorUi.otherVisitorOrder.addDataVisitorFilter();
@@ -67,6 +76,7 @@ tests.visitorUi.otherVisitorOrder.deleteDataVisitorFilter();
 
 // Бюро пропусков / Посетители - вкладка - Действующие
 tests.visitorUi.otherVisitorActive.addDataVisitorFilter();
+decorate.page.base.refresh();
 tests.visitorUi.otherVisitorActive.divisionFilterVisitor();
 tests.visitorUi.otherVisitorActive.searchFilterVisitorActive();
 tests.visitorUi.otherVisitorActive.deleteDataVisitorFilter();
@@ -76,27 +86,32 @@ data.deleteDataVisitor();
 // Бюро пропусков / Сотрудники
 // Бюро пропусков / Сотрудники
 data.addDataStaff();
+decorate.page.base.refresh();
 tests.staffUi.otherPass.addStaffMinParams();
 tests.staffUi.otherPass.addCardStaffMinParams();
 tests.staffUi.otherPass.deleteCardStaffMinParams();
 tests.staffUi.otherPass.blockCardStaffMinParams();
 tests.staffUi.otherPass.unblockCardStaffMinParams();
+decorate.page.base.refresh();
 tests.staffUi.otherPass.addBarcodeStaffMinParams();
 tests.staffUi.otherPass.deleteBarcodeStaffMinParams();
 tests.staffUi.otherPass.editStaffMaxParams();
 tests.staffUi.otherPass.printTableStaffMaxParams();
 tests.staffUi.otherPass.printBarcodeStaffMaxParams();
+decorate.page.base.refresh();
 tests.staffUi.otherPass.printDesignCardStaffMaxParams();
 tests.staffUi.otherPass.dimissedRestoreStaffMinParams();
 tests.staffUi.otherPass.exportStaffXLSXMaxParams();
 tests.staffUi.otherPass.exportStaffCSVMaxParams();
 tests.staffUi.other.dimissedStaff();
 tests.staffUi.other.deleteStaff();
+decorate.page.base.refresh();
 
 data.addDataStaffFilter();
 tests.staffUi.otherPass.searchFilterStaffActive();
 tests.staffUi.otherPass.divisionFilterStaffActive();
 tests.staffUi.otherPass.cardSearchStaffActive();
 data.deleteDataStaffFilter();
+decorate.page.base.refresh();
 
 closeBrowser();
